3c391b10d5aa097d04a93873b0465447a4508068
Олег Майоров <70757056+linfer-developer@users.noreply.github.com>
2026-07-19 20:12:09 +0300

    Delete pysevsu/pysevsu directory

--- pysevsu/pysevsu/models/types.py
+++
@@ -1,7 +0,0 @@
-from typing import Dict, Any, Optional
-from pysevsu.models.enums import ScheduleSource, WeekFields, LessonFields
-
-
-WebsiteData = Dict[ScheduleSource, Optional[Any]]
-WeekData = Dict[WeekFields, Optional[Any]]
-LessonData = Dict[LessonFields, Optional[Any]]
